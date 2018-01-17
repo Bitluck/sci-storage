b8773b11879b6841fe07008ebc4ec5c702f3c894
Schukin Andrei <bitluck11@gmail.com>
2018-01-17 19:34:39 +0600

    Add realization of the BaseDAL class

--- src/java/DAL/BaseDAL.java
+++ src/java/DAL/BaseDAL.java
@@ -5,10 +5,28 @@
  */
 package DAL;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
 /**
  *
  * @author Администратор
  */
 public class BaseDAL {
-    
+    protected SqlSessionFactory sqlSessionFactory;
+
+    public BaseDAL() {
+        Reader reader = null;
+        try {
+            reader = Resources.getResourceAsReader("mybatis/config.xml");
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        } catch (IOException ex) {
+            Logger.getLogger(BaseDAL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
